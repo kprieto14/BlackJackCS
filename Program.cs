1dae31a5db4c78741b31bc5ca9005531980fb628
Kristy Prieto <kprieto14@gmail.com>
2023-02-28 21:36:03 -0500

    Finished most of the blackjack game, will be adding feature to restart game by reading player's response next.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,556 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace BlackJackCS
 {
+  class Card 
+    {
+      public string Face {get; set;}
+      public string Suit {get; set;}
+      public int Value {get; set;}
+    }
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      Console.WriteLine();
+      Console.WriteLine("Welcome to the game of Blackjack! I will be your dealer. Lets shuffle the cards and begin!");
+      Console.WriteLine();
+    
+      //Creates the deck that assigns a Suit, face, value to each 52 cards. 
+      var deck = new List<Card>()
+      {
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "Ace",
+            Value = 11
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "2",
+            Value = 2
+          }, 
+
+        new Card()
+         {
+           Face = "Clubs",
+            Suit = "3",
+            Value = 3
+          },
+
+        new Card()
+         {
+           Face = "Clubs",
+           Suit = "4",
+           Value = 4
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "5",
+            Value = 5
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "6",
+            Value = 6
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "7",
+            Value = 7
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "8",
+            Value = 8
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "9",
+            Value = 9
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "10",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "Jack",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "Queen",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Clubs",
+            Suit = "King",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "Ace",
+            Value = 11
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "2",
+            Value = 2
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "3",
+            Value = 3
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "4",
+            Value = 4
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "5",
+            Value = 5
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "6",
+            Value = 6
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "7",
+            Value = 7
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "8",
+            Value = 8
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "9",
+            Value = 9
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "10",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "Jack",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "Queen",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Diamonds",
+            Suit = "King",
+            Value = 10
+          },
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "Ace",
+            Value = 11
+          },
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "2",
+            Value = 2
+          },
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "3",
+            Value = 3
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "4",
+            Value = 4
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "5",
+            Value = 5
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "6",
+            Value = 6
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "7",
+            Value = 7
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "8",
+            Value = 8
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "9",
+            Value = 9
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "10",
+            Value = 10
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "Jack",
+            Value = 10
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "Queen",
+            Value = 10
+          },  
+
+        new Card()
+          {
+            Face = "Hearts",
+            Suit = "King",
+            Value =10
+          },
+        
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "Ace",
+            Value = 11
+          },
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "2",
+            Value = 2
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "3",
+            Value = 3
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "4",
+            Value = 4
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "5",
+            Value = 5
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "6",
+            Value = 6
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "7",
+            Value = 7
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "8",
+            Value = 8
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "9",
+            Value = 9
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "10",
+            Value = 10
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "Jack",
+            Value = 10
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "Queen",
+            Value = 10
+          }, 
+
+        new Card()
+          {
+            Face = "Spades",
+            Suit = "King",
+            Value = 10
+          }, 
+         
+                            
+      };
+
+      //Establishes loop that will shuffle deck
+      for(var rightIndex = deck.Count - 1; rightIndex >=1; rightIndex--) 
+      {
+        //Establishes the 2nd card to be randomly shuffled from the last card in deck
+        var randomNumberGenerator = new Random();
+        var leftIndex = randomNumberGenerator.Next(rightIndex);
+        //Matches the number index from loop and re-assigns the value with the corresponding value from the "deck" list
+        var leftCard = deck[leftIndex];
+        var rightCard = deck[rightIndex];
+        //Switches the left card with the right card to be shuffled until the loop reaches the last card
+        deck[rightIndex] = leftCard;
+        deck[leftIndex] = rightCard;
+      }
+
+      //Creates new lists to add cards into deck for PlayerOne and PlayerTwo
+      var playerHand = new List<Card> ();
+      var dealerHand = new List<Card> ();
+
+      //Take the first two cards from the deck, copy them into playerHand (deleting the first two cards from the deck afterwards)
+      for (var count = 0; count < 2; count ++)
+      {
+        playerHand.Add(deck[0]);
+        deck.Remove(deck[0]);
+      }
+      //Repeat process with dealer
+       for (var count = 0; count < 2; count ++)
+      {
+        dealerHand.Add(deck[0]);
+        deck.Remove(deck[0]);
+      }
+
+      var firstCard = playerHand[0];
+      var secondCard = playerHand[1];
+
+      Console.WriteLine($"You have a {firstCard.Suit} of {firstCard.Face} and a {secondCard.Suit} of {secondCard.Face}.");
+   
+      //Process that starts calculating the player total and asking if they would like to HIT or STAND when under 21.
+      bool endTurn = false;
+      while (endTurn == false)
+      {
+        //Console.WriteLine($"You have the current hand of {playerHand.Select(card => card.Face, card.Suit)}"); Fix this code later to display what cards are in player hands
+        var newPlayerTotal = playerHand.Select(sum => sum.Value).Sum();
+        Console.WriteLine($"Your total value is {newPlayerTotal}.");
+        Console.WriteLine();
+       
+        if (newPlayerTotal < 21)
+        {
+          Console.WriteLine("Would you like to HIT or STAND?");
+          var response = Console.ReadLine();
+
+          if (response.ToUpper() == "HIT")
+          {
+            playerHand.Add(deck[0]);
+            deck.Remove(deck[0]);
+
+            var newCard = playerHand[playerHand.Count - 1];
+            Console.WriteLine($"You drew a {newCard.Face} of {newCard.Suit}.");
+          }
+          else if(response.ToUpper() == "STAND")
+          {
+            Console.WriteLine($"Your total value was {newPlayerTotal}! Let's see what the dealer has.");
+            Console.WriteLine();
+            endTurn = true; 
+          }
+          else
+          {
+            Console.WriteLine("I'm sorry, I do not understand. Please try again.");
+          }
+        }
+        else if (newPlayerTotal == 21)
+        {
+          Console.WriteLine("Congrats on the perfect 21! Let's see what the dealer has.");
+          Console.WriteLine();
+          endTurn = true; 
+        }
+        else if(newPlayerTotal > 21)
+        {
+          Console.Write("💣💣💣");
+          Console.WriteLine(" Uh oh, looks like a bust!");
+          Console.WriteLine();
+          endTurn = true; 
+        }
+        else
+        {
+          Console.WriteLine("An error has occurred. Please contact Kristy to fix the code.");
+        }
+      }
+
+      var dealerFirstCard = dealerHand[0];
+      var dealerSecondCard = dealerHand[1];
+    
+      Console.WriteLine($"Dealer has a {dealerFirstCard.Suit} of {dealerFirstCard.Face} and a {dealerSecondCard.Suit} of {dealerSecondCard.Face}.");
+      
+      //Process that starts the dealer's turn and calculates their total.
+      bool dealerTurn = true;
+      while (dealerTurn == true)
+      {
+        var newDealerTotal = dealerHand.Select(sum => sum.Value).Sum();
+
+        Console.WriteLine($"The dealer currently has a value of {newDealerTotal}.");
+        Console.WriteLine();
+        if (newDealerTotal >= 17 && newDealerTotal < 21)
+        {
+          Console.WriteLine("Dealer chooses to stand.");
+          Console.WriteLine();
+          dealerTurn = false;
+        }
+        else if (newDealerTotal == 21)
+        {
+          Console.WriteLine("What terrible luck for you.");
+          Console.WriteLine();
+          dealerTurn = false;
+        }
+        else if (newDealerTotal > 21)
+        {
+          Console.Write("💣💣💣");
+          Console.WriteLine(" Uh oh, looks like a bust for the dealer!");
+          Console.WriteLine();
+          dealerTurn = false;
+        }
+        else 
+        {
+          dealerHand.Add(deck[0]);
+          deck.Remove(deck[0]);
+
+          var newCard = dealerHand[dealerHand.Count - 1];
+          Console.WriteLine($"Dealer chooses to hit, adding a {newCard.Suit} of {newCard.Face} to their hand.");
+        }
+      }
+
+      //The start of the sum of each hand to compare and gives a victory speech at the end according to whoever won.
+      var playerTotal = playerHand.Select(sum => sum.Value).Sum();
+      var dealerTotal = dealerHand.Select(sum => sum.Value).Sum();
+
+      if (playerTotal > dealerTotal && playerTotal <= 21)
+      {
+        Console.WriteLine("Congrats player!");
+      }
+      else if (playerTotal < dealerTotal && dealerTotal <= 21)
+      {
+        Console.WriteLine("Congrats dealer!");
+      }
+      else if (playerTotal == dealerTotal && dealerTotal <= 21)
+      {
+        Console.WriteLine("Congrats dealer!");
+      }
+      else if (playerTotal > 21)
+      {
+         Console.WriteLine("Congrats dealer!");
+      }
+      else if (playerTotal < dealerTotal && dealerTotal > 21)
+      {
+        Console.WriteLine("Congrats player!");
+      }
+      else
+      {
+        Console.WriteLine("Something went wrong, please contact Kristy to fix code.");
+      }
+
+      /*
+
+      */ 
+
     }
   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,122 @@
 # BlackJackCS
+P
+
+Will create a program that replicates a 2-player game of Blackjack (aka 21) using one deck of 52 cards. After shuffling the deck, 
+2 cards will be given (from the shuffled deck) to the player and 2 cards (from the same deck) to the "dealer". The player will be able to see their hand, but not be able to see the dealers hand until the end.
+Each card will have its own value, which will be added for a total value in each hand. The hand with the highest number without going over 21 is considered to win (a tie makes the dealer win). The player and dealer take turns on whether they want to hit (take another card from the deck) or stand (keep their current hand).
+The player will go first in deciding if they want to "hit" or "stand". If player chooses to "hit" and they are still below the value of 21, they may chose to "hit" again or "stand". This process repeats until the player either chooses to stand or receives a card that brings their total over 21, which automatically makes the player lose. 
+Whether the player choses to stand or or loses, the dealer's turn begins and reveals their cards. If the dealer has a value under 17, they keep "hit" until their value goes over 17 (unless they go over 21, in which case they lose)
+If both the player and dealer tie, the dealer wins. 
+The player will be given the option to play again, and if they choose yes then the cards are taken from each hand and re-shuffled to start another round.
+
+E
+
+1. If player one has a 6 of hearts and a 8 of clubs, then hits with a 2 of spades, then stands for a total value of 16. AND if dealer has a 4 of spades and jack of diamonds, 
+    then hits with a Queen of Hearts for a total value of 24. the dealer busts and the player wins.
+2. If player has a Jack of clubs and a Ace of diamonds, then stands for a total value of 21 (perfect score). AND if dealer has a 6 of clubs and 5 of spades, 
+    then hits for a 9 of clubs, then stands for a total value of 20. Then the player wins for having 21. 
+3. If player draws a 3 of hearts and 10 of hearts, then draws a 3 of diamonds, then draws a 4 of hearts, then stands for a total value of 20. AND the dealer draws a Queen of diamonds
+    and a 6 of clubs and a 5 of spades, then stands for a total value of 21. Then the dealer wins for having 21.
+4. If player draws a 4 of diamonds and a Jack of hearts, then hits for a Ace of clubs, then the player automatically busts and the dealer goes. If the dealer draws a 3 of clubs,
+    a 5 of diamonds, 4 of spades, and a Jack of diamonds for a total value of 22. Then the dealer also busts but since its a tie then the dealer wins.
+5. If the player draws a 6 of clubs and a 5 of spades, then hits for a 9 of clubs and then stands for a total value of 20. AND the dealer draws a 10 of clubs and a 7 of diamonds
+    then stands for a total value of 17. The player wins for having a higher value that was not over 21. 
+6. If the player draws a Ace of spades and a 7 of Hearts, then stands for a total value of 18. AND the dealer draws a 5 of Diamonds and a 9 of clubs, then draws a 5 of spades
+    and stands for a value of 19. Then the dealer wins for having a higher value under 21.
+
+D
+
+Will have classes
+    1 class of cards which will have 3 properties
+        suit
+        face
+        value
+    1 list/ class which will be deck
+    1 list/ class for playerHand
+    1 list/ class for dealerHand
+
+Use looping to shuffle deck
+Using while statement (endTurn = false)
+    { Calculate total of hand
+        If value of card is under 21, hit or stand
+        if hit, draw card and go back to step one to calculate total and adjust accordingly
+        
+        If value is over 21, automatically end turn and insert explosive emoji.
+
+        If player stands, end turn and repeat with dealerHand (can turn this into a method where it sends the hand information with values and returns a total value for comparison later?)
+    }
+While statement to ask player if they would like to play again and loop accordingly (playAgain = true)
+Variables to accept player's responses throughout the game
+    var response = Console.ReadLine(toUpper(Y or N)); (perhaps can use this method to call an prompt from main method and return the value of response)
+Variables to calculate the totals and compare them with IF statements
+    double totalPlayerHand
+    double totalDealerHand
+Switch statement at the end which compares values and returns a dialogue that matches who won based on values.
+
+A
+
+Greeting!
+
+Shuffle deck using looping
+    Assign 4 different suits
+    Assign 13 ranks to each suit
+    Give value to each ranks
+        Jacks/ Queens/ Kings = 10
+        Aces = 11
+        All other numbers will correspond accordingly (ie 2=2, 3=3, 4=4, etc...)
+
+Take the first two cards from the deck, copy them into playerHand (deleting the first two cards from the deck afterwards)
+    Repeat process with dealer
+
+while statements (endTurn = false) with playerHand
+    { Calculate total of hand
+        If value of card is under 21, hit or stand
+
+        If card is automatically 21, end loop
+
+        If hit, draw card from deck (removing the card from the deck) and add into hand go back to step one to calculate total and adjust accordingly
+        
+        If the value is over 21, automatically end turn and insert explosive emoji.
+
+        If player stands, end turn (can turn this into a method where it sends the hand information with values and returns a total value for comparison later?)
+    
+        else repeat prompt
+    }
+
+while statements (endTurn = false) with dealerHand
+    { Calculate total of hand
+        If value of card is under 17, hit
+
+        If card is automatically 21, end loop
+
+        If hit, draw card from deck (removing the card from the deck) and add into hand go back to step one to calculate total and adjust accordingly
+        
+        If the value is over 21, automatically end turn and insert explosive emoji.
+
+        When value is greater than or equal to 17, the dealer stands, ends (can turn this into a method where it sends the hand information with values and returns a total value for comparison later?)
+    }
+
+switch statement for different scenarios (dealer really has the upper hand huh)
+    A: totalPlayerHand > totalDealerHand && totalPlayerHand <= 21
+        Console.WriteLine(Congrats player!);
+    B: totalPlayerHand < totalDealerHand && totalDealerHand <= 21 
+        Console.WriteLine(Congrats dealer!);
+    C: totalPlayerHand == totalDealerHand && totalDealerHand <= 21
+        Console.WriteLine(Congrats dealer!);
+    D: totalPlayerHand > 21
+        Console.WriteLine(Congrats dealer!);
+    E: totalPlayerHand < totalDealerHand && totalDealerHand > 21
+        Console.WriteLine(Congrats player!);
+
+clear playerHand and dealerHand
+
+While statement (playAgain == true)
+{
+    Console.Write(Would you like to play again? Y or N)
+    if Y, restart code, starting from shuffle deck
+
+    if N, end code
+
+    else repeat prompt 
+}
+C
